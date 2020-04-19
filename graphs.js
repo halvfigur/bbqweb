7c32abf47dd7dce76664d612295869c13bbbe783
Alex <alex@hitch>
2020-04-19 19:39:24 +0200

    Better palette

--- graphs.js
+++ graphs.js
@@ -3,6 +3,7 @@ var canvas = document.getElementById("myCanvas");
 var chart = Chart.Line(canvas,{
 	data:{
 		datasets: [
+			/*
 			{
 				label: "Probe1", 
 				fill: false,
@@ -16,6 +17,8 @@ var chart = Chart.Line(canvas,{
 
 				borderWidth: 1
 			},
+			*/
+			/*
 			{
 				label: "Probe2", 
 				fill: false,
@@ -81,6 +84,7 @@ var chart = Chart.Line(canvas,{
 
 				borderWidth: 1
 			}
+			*/
 		],
 	},
 	options:{
@@ -105,8 +109,41 @@ var chart = Chart.Line(canvas,{
 	}
 });
 
+var colors = [
+	//#987284
+	//#75B9BE
+	//#D0D6B5
+	//#F9B5AC
+	//#EE7674
+	//#4D5382
+	{
+		background: 'rgba(152, 114, 132, 0.2)',
+		border: 'rgba(152, 114, 132, 1)',
+	},
+	{
+		background: 'rgba(117, 185, 190, 0.2)',
+		border: 'rgba(117, 185, 190, 1)',
+	},
+	{
+		background: 'rgba(208, 214, 181, 0.2)',
+		border: 'rgba(208, 214, 181, 1)',
+	},
+	{
+		background: 'rgba(249, 181, 172, 0.2)',
+		border: 'rgba(249, 181, 172, 1)',
+	},
+	{
+		background: 'rgba(238, 118, 116, 0.2)',
+		border: 'rgba(238, 118, 116, 1)',
+	},
+	{
+		background: 'rgba(77, 83, 130, 0.2)',
+		border: 'rgba(77, 83, 130, 1:)',
+	},
+];
 
 var maxLen = 10;
+var measurements = 0;
 
 var ws = new WebSocket("ws://localhost:9000");
 
@@ -125,29 +162,54 @@ ws.onmessage = function(evt) {
 	let payload = JSON.parse(evt.data);
 	let temps = payload.Temperatures;
 	let t = payload.T;
-	let ds = chart.data.datasets;
+	//let ds = chart.data.datasets;
 
 	for (let i = 0; i < temps.length; i++) {
 		let temp = temps[i];
 
-		// -32768 is the smallest unsigned 16 bit integer and means
-		// there's no data available for this probe at this time.
-		if (temp === -32768) {
+		let label = 'Probe' + (i + 1);
+		let dsi = chart.data.datasets.findIndex((x) => x.label == label);
+		let ds = null;
+
+		if (dsi != -1) {
+			ds = chart.data.datasets[dsi];
+		} else {
+			let color = colors[i];
+
+			ds = {
+				label: label, 
+				fill: false,
+				data: [],
+				backgroundColor: [
+					//'rgba(255, 99, 132, 0.2)',
+					color.background,
+				],
+				borderColor: [
+					//'rgba(255, 99, 132, 1)',
+					color.border,
+				],
+
+				borderWidth: 1
+			}
+
+			chart.data.datasets.push(ds);
+		}
+
+		if (ds.data.length == maxLen) {
+			ds.data.shift();
+		}
+
+		if (temp == -32768) {
 			continue;
 		}
 
-		console.log('ds[i].length = maxLen', ds[i].data.length, ' == ', maxLen);
-		if (ds[i].data.length == maxLen) {
-			ds[i].data.shift();
-		}
-
-		ds[i].data.push({
+		ds.data.push({
 			x: t,
 			y: temp
 		});
-
-		chart.update();
 	}
+
+	chart.update();
 }
 
 ws.onerror = function(evt) {
