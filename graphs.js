fa6255f3dfd8153b3173dc977514e3cb38fa4ee6
Alex <alex@hitch>
2020-04-19 07:21:24 +0200

    Use websockets instead

--- graphs.js
+++ graphs.js
@@ -105,90 +105,51 @@ var chart = Chart.Line(canvas,{
 	}
 });
 
+
 var maxLen = 10;
-var prevHour = moment().subtract(100, 'hours').utc().valueOf();
-fetch('http://localhost:8086/query?db=bbq&q='+encodeURIComponent('SELECT * FROM temperature WHERE time > ' + prevHour))
-	.then((response) => {
-		return response.json();
-	})
-	.then((data) => {
-		let cols = data.results[0].series[0].columns;
-		let tIdx = -1;
-		let p1Idx = -1;
-		let p2Idx = -1;
-		let p3Idx = -1;
-		let p4Idx = -1;
-		let p5Idx = -1;
-		let p6Idx = -1;
 
-		for (let i = 0; i < cols.length; i++) {
-			switch (cols[i]) {
-				case "time":
-					tIdx = i;
-					break;
-				case "probe1":
-					p1Idx = i;
-					break;
-				case "probe2":
-					p2Idx = i;
-					break;
-				case "probe3":
-					p3Idx = i;
-					break;
-				case "probe4":
-					p4Idx = i;
-					break;
-				case "probe5":
-					p5Idx = i;
-					break;
-				case "probe6":
-					p6Idx = i;
-					break;
-			}
+var ws = new WebSocket("ws://localhost:9000");
+
+ws.onopen = function(evt) {
+	console.log("OPEN");
+}
+
+ws.onclose = function(evt) {
+	console.log("CLOSE");
+	ws = null;
+}
+
+ws.onmessage = function(evt) {
+	console.log("RESPONSE: " + evt.data);
+
+	let payload = JSON.parse(evt.data);
+	let temps = payload.Temperatures;
+	let t = payload.T;
+	let ds = chart.data.datasets;
+
+	for (let i = 0; i < temps.length; i++) {
+		let temp = temps[i];
+
+		// -32768 is the smallest unsigned 16 bit integer and means
+		// there's no data available for this probe at this time.
+		if (temp === -32768) {
+			continue;
 		}
 
-		let vals = data.results[0].series[0].values;
-
-		let from = 0 ? vals.length <= maxLen : vals.length - maxLen;
-		for (let i = from; i < vals.length; i++) {
-			
-			let v = vals[i];
-			let t = new Date(v[tIdx]);
-			let p = [
-				v[p1Idx],
-				v[p2Idx],
-				v[p3Idx],
-				v[p4Idx],
-				v[p5Idx],
-				v[p6Idx]
-			];
-
-			for (let j = 0; j < p.length; j++) {
-				if (p[j] === -32768) {
-					continue;
-				}
-
-				chart.data.datasets[j].data.push({
-					x: t,
-					y: p[j]
-				});
-			}
+		console.log('ds[i].length = maxLen', ds[i].data.length, ' == ', maxLen);
+		if (ds[i].data.length == maxLen) {
+			ds[i].data.shift();
 		}
+
+		ds[i].data.push({
+			x: t,
+			y: temp
+		});
+
 		chart.update();
-		console.log(data);
-	});
-
-/*
-var y = 0;
-setInterval(function() {
-	if (chart.data.datasets[0].data.length > maxLen) {
-		chart.data.datasets[0].data.shift();
 	}
+}
 
-	chart.data.datasets[0].data.push({
-		x: new Date(),
-		y: y++
-	});
-	chart.update();
-}, 1000);
-*/
+ws.onerror = function(evt) {
+	console.log("ERROR: " + evt.data);
+}
